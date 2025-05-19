da433b1e587171e869384497edda2be7185e2c18
kavya <kavya.n@apcfss.in>
2025-05-19 16:35:29 +0530

    Frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignInPage from './Components/SignInPage';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+          <SignInPage/>
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
