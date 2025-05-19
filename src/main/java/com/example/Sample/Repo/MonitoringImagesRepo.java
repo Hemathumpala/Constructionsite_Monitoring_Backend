d1d919c1c51ae300d91b1e31d94ecab043a71375
kavya <kavya.n@apcfss.in>
2025-05-19 16:31:51 +0530

    Remove all files from master

--- src/main/java/com/example/Sample/Repo/MonitoringImagesRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Sample.Repo;
-
-import com.example.Sample.Entity.MonitoringImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonitoringImagesRepo extends JpaRepository<MonitoringImages,Integer> {
-
-    MonitoringImages findById(int id);
-}
